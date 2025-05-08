1ec4b7eb65f1144b2f3edb597e20f4675d6dc50c
sprunk31 <jerrymarges1986@gmail.com>
2025-05-08 21:21:39 +0200

    2.0

--- main.py
+++ main.py
@@ -336,7 +336,7 @@ with tab2:
     get_position='[r_lon, r_lat]',
     get_fill_color=kleur_map[route],
     radiusMinPixels=3,
-    radiusMaxPixels=6,
+    radiusMaxPixels=8,
     pickable=True,
     get_line_color=[0, 0, 0],
     line_width_min_pixels=0
